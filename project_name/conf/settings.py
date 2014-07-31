385ad56c860e68c75790340eea98b4f4ccca2aef
Manuel García <mgarcia@culturaweb.com>
2014-07-31 18:15:43 +0200

    Added SITE_ID = 1

--- project_name/conf/settings.py
+++ project_name/conf/settings.py
@@ -122,3 +122,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+
+
+# -----------------------------------------------------------------------------
+# Sites Framework
+# -----------------------------------------------------------------------------
+
+SITE_ID = 1
